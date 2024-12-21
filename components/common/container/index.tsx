f2dc3a483114b6387d7c2209668394fc507713b7
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-12-21 16:22:15 +0900

    feat: 공통 Container 컴포넌트 구현

---
+++ components/common/container/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ScrollView, ScrollViewProps, StyleSheet, View, ViewProps } from "react-native";
+
+type ContainerProps<T extends "View" | "ScrollView"> = {
+  children: React.ReactNode;
+  as: T;
+} & (T extends "View" ? ViewProps : ScrollViewProps);
+
+export default function Container<T extends "View" | "ScrollView">({
+  children,
+  as,
+  ...restProps
+}: ContainerProps<T>) {
+  const Component = as === "View" ? View : ScrollView;
+
+  return (
+    <Component style={styles.container} {...(restProps as any)}>
+      {children}
+    </Component>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 20,
+    paddingHorizontal: 24,
+  },
+});
