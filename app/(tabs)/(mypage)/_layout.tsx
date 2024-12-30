cd79e45236106f8aa0a76b85883345a2a44caec8
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-12-30 17:02:49 +0900

    feat: 가게 추가하기 라우팅 추가

--- app/(tabs)/(mypage)/_layout.tsx
+++ app/(tabs)/(mypage)/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 
+import BackHeader from "@/components/common/header/back-header";
 import MainHeader from "@/components/common/header/main-header";
 
 export default function MypageLayout() {
@@ -8,6 +9,10 @@ export default function MypageLayout() {
       <Stack.Screen
         name="index"
         options={{ header: () => <MainHeader title="마이페이지" type="mypage" /> }}
+      />
+      <Stack.Screen
+        name="add"
+        options={{ header: () => <BackHeader title="가게 등록하기" type="primary" /> }}
       />
     </Stack>
   );
---
+++ app/(tabs)/(mypage)/add.tsx
@@ -0,0 +1,5 @@
+import Text from "@/components/common/text";
+
+export default function StoreAddScreen() {
+  return <Text>Store Add Screen</Text>;
+}
