590ec7d73b25eeca4fd98711d78e6f84dc29db45
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-01-20 13:50:56 +0900

    [feat] 채팅, 주소 검색 기능 구현 (#33)
    * feat: 주소 검색 기능 구현
    
    * feat: 가게 위치 보여주는 지도 구현
    
    * feat: 채팅 기능 구현

---
+++ src/hooks/use-chat.ts
@@ -0,0 +1,85 @@
+import { useEffect, useRef, useState } from "react";
+
+import { Client, IFrame, IMessage, StompConfig } from "@stomp/stompjs";
+
+const socketEndpoint = process.env.EXPO_PUBLIC_CHAT_URL;
+
+export const useChat = (
+  chatRoomId: number,
+  receiverId: number,
+  setMessageList: (message: React.SetStateAction<string[]>) => void
+) => {
+  const [message, setMessage] = useState("");
+  const [hasConnection, setConnection] = useState(false);
+
+  const client = useRef<Client | null>(null);
+
+  const sendMessage = () => {
+    if (message.trim() === "") return;
+
+    client.current?.publish({
+      destination: `/app/${chatRoomId}`,
+      body: JSON.stringify({
+        chatRoomId,
+        senderId: 1,
+        receiverId,
+        message: message,
+      }),
+    });
+
+    setMessage("");
+  };
+
+  const onChangeText = (e: string) => {
+    setMessage(e);
+  };
+
+  const disConnect = () => {
+    if (client.current === null) return;
+
+    client.current?.deactivate();
+  };
+
+  const callback = function (message: IMessage) {
+    if (message.body) {
+      const msg = JSON.parse(message.body);
+      setMessageList((prev) => [...prev, msg.result.message]);
+    }
+  };
+
+  useEffect(() => {
+    const stompConfig: StompConfig = {
+      brokerURL: socketEndpoint,
+      debug: (frame: any) => console.log(frame),
+
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
+      forceBinaryWSFrames: true,
+      appendMissingNULLonIncoming: true,
+    };
+
+    if (!client.current) {
+      client.current = new Client(stompConfig);
+      client.current.onConnect = () => {
+        setConnection(true);
+        console.log(`connected to ${stompConfig.brokerURL}`);
+        client.current?.subscribe(`/topic/${chatRoomId}`, callback);
+      };
+      client.current.onDisconnect = (error: IFrame) => {
+        console.log(`disconnected to  ${stompConfig.brokerURL}`);
+        console.log(error);
+      };
+      client.current.onDisconnect = (error: IFrame) => {
+        console.log(`error to  ${stompConfig.brokerURL}`);
+        console.log(error);
+      };
+
+      client.current?.activate();
+    }
+
+    return () => disConnect();
+  }, [chatRoomId]);
+
+  return { message, onChangeText, sendMessage, hasConnection };
+};
