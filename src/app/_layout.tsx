691093cce260b25294f39727e34108c47baf2c23
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2025-02-03 15:32:51 +0900

    refactor: root provider 컴포넌트로 분리

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,93 +1,30 @@
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-
-import { useEffect } from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-
-// import messaging from "@react-native-firebase/messaging";
-// import NaverLogin from "@react-native-seoul/naver-login";
-// import { GoogleSignin } from "@react-native-google-signin/google-signin";
-// import { initializeKakaoSDK } from "@react-native-kakao/core";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import BackHeader from "@/src/components/common/header/back-header";
 import SearchHeader from "@/src/components/search/header";
 import { couponStacks } from "@/src/components/stacks/coupon";
 import { eventStacks } from "@/src/components/stacks/event";
 
-// import { requestUserPermission } from "../utils/fcm";
-
-SplashScreen.preventAutoHideAsync();
-
-const queryClient = new QueryClient();
-
-const consumerKey = process.env.EXPO_PUBLIC_NAVER_CLIEND_ID || "";
-const consumerSecret = process.env.EXPO_PUBLIC_NAVER_CLIEND_SECRET || "";
-const appName = process.env.EXPO_PUBLIC_NAVER_APP_NAME || "";
-const serviceUrlSchemeIOS = process.env.EXPO_PUBLIC_APP_BUNDLE_IDENTIFIER || "";
-const kakaoNativeAppKey = process.env.EXPO_PUBLIC_NATIVE_APP_KEY || "";
-const iosClientId = process.env.EXPO_PUBLIC_GOOGLE_CLIEND_ID || "";
+import RootProvider from "../components/provider/root-provider";
 
 export const unstable_settings = {
   initialRouteName: "(tabs)/(event)/index",
 };
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/PretendardVariable.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-      // NaverLogin.initialize({
-      //   appName,
-      //   consumerKey,
-      //   consumerSecret,
-      //   serviceUrlSchemeIOS,
-      //   disableNaverAppAuthIOS: true,
-      // });
-      // GoogleSignin.configure({
-      //   iosClientId,
-      // });
-      // initializeKakaoSDK(kakaoNativeAppKey);
-
-      // const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      //   console.log("FCM Notification Received:", remoteMessage);
-      // });
-
-      // (async () => {
-      //   await requestUserPermission();
-      // })();
-
-      // return () => {
-      //   unsubscribe();
-      // };
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <GestureHandlerRootView>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            {eventStacks()}
-            {couponStacks()}
-            <Stack.Screen name="search" options={{ header: () => <SearchHeader /> }} />
-            <Stack.Screen name="alarm" options={{ header: () => <BackHeader title="알림" /> }} />
-            <Stack.Screen name="address" options={{ headerShown: false }} />
-            <Stack.Screen name="chat/[id]" options={{ headerShown: false }} />
-          </Stack>
-        </GestureHandlerRootView>
-      </QueryClientProvider>
+    <RootProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {eventStacks()}
+        {couponStacks()}
+        <Stack.Screen name="search" options={{ header: () => <SearchHeader /> }} />
+        <Stack.Screen name="alarm" options={{ header: () => <BackHeader title="알림" /> }} />
+        <Stack.Screen name="address" options={{ headerShown: false }} />
+        <Stack.Screen name="chat/[id]" options={{ headerShown: false }} />
+      </Stack>
       <StatusBar style="auto" />
-    </>
+    </RootProvider>
   );
 }
---
+++ src/components/provider/root-provider.tsx
@@ -0,0 +1,69 @@
+import { useFonts } from "expo-font";
+import { SplashScreen } from "expo-router";
+
+import { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import messaging from "@react-native-firebase/messaging";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { initializeKakaoSDK } from "@react-native-kakao/core";
+import NaverLogin from "@react-native-seoul/naver-login";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { requestUserPermission } from "@/src/utils/fcm";
+
+SplashScreen.preventAutoHideAsync();
+
+const queryClient = new QueryClient();
+
+const consumerKey = process.env.EXPO_PUBLIC_NAVER_CLIEND_ID || "";
+const consumerSecret = process.env.EXPO_PUBLIC_NAVER_CLIEND_SECRET || "";
+const appName = process.env.EXPO_PUBLIC_NAVER_APP_NAME || "";
+const serviceUrlSchemeIOS = process.env.EXPO_PUBLIC_APP_BUNDLE_IDENTIFIER || "";
+const kakaoNativeAppKey = process.env.EXPO_PUBLIC_NATIVE_APP_KEY || "";
+const iosClientId = process.env.EXPO_PUBLIC_GOOGLE_CLIEND_ID || "";
+
+export default function RootProvider({ children }: { children: React.ReactNode }) {
+  const [loaded] = useFonts({
+    SpaceMono: require("../../assets/fonts/PretendardVariable.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+      NaverLogin.initialize({
+        appName,
+        consumerKey,
+        consumerSecret,
+        serviceUrlSchemeIOS,
+        disableNaverAppAuthIOS: true,
+      });
+      GoogleSignin.configure({
+        iosClientId,
+      });
+      initializeKakaoSDK(kakaoNativeAppKey);
+
+      const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+        console.log("FCM Notification Received:", remoteMessage);
+      });
+
+      (async () => {
+        await requestUserPermission();
+      })();
+
+      return () => {
+        unsubscribe();
+      };
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView>{children}</GestureHandlerRootView>
+    </QueryClientProvider>
+  );
+}
